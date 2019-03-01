5e3c496f419f1862962179f9f26a826ca5762f28
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2019-02-28 21:39:27 -0500

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { AppRouter } from './App.router';
+import { NavBar } from './navbar/Navbar';
+import { Home } from './home/Home';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { About } from './about/About';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <AppRouter />
-        </header>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <NavBar />
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+            <Route path="/" exact component={Home} />
+            <Route path="/about" component={About} />
+          </header>
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/about/About.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const About = () => (
+    <div>
+        <h1>About This</h1>
+        <h2>He Attacc</h2>
+        <h3>He Protecc</h3>
+        <h4>But most importantly,</h4>
+        <h1><b>HE REACC</b></h1>
+    </div>
+);
