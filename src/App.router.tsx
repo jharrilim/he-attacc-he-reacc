8a76b7c0258d78a81f8e4f90adb57cfa5b248657
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2019-03-01 18:59:43 -0500

    Remove app router

--- src/App.router.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import { Home } from './components/home/Home';
-
-export const AppRouter = () => (
-    <Router>
-        <Route path="/" exact component={Home} />
-    </Router>
-);
